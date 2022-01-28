c3d905b820b4aca761d392c07151e19ea5c67e5b
lucasalmeida-egsys <77992211+lucasalmeida-egsys@users.noreply.github.com>
2022-01-28 20:06:41 -0300

    Fill screen switcher

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+    @SCREEN
+    D=A
+
+    @screenAddr
+    M=D
+
+    @i
+    M=0
+
+    @8192
+    M=A
+    D=M
+
+    @max
+    M=D
+
+    @KBD
+    D=M;
+
+    @DRAW
+    D; JGT
+
+    @CLEAR
+    0; JMP
+
+(DRAW)
+    @i
+    D=M;
+
+    @screenAddr
+    A=M+D;
+    M=-1
+
+    @i
+    M=D+1
+
+    @max
+    M=M-1
+    D=M;
+
+    @DRAW
+    D; JGT
+
+    @LOOP
+    0; JMP
+
+(CLEAR)
+    @i
+    D=M;
+
+    @screenAddr
+    A=M+D;
+    M=0
+
+    @i
+    M=D+1
+
+    @max
+    M=M-1
+    D=M;
+
+    @CLEAR
+    D; JGT
+
+    @LOOP
+    0; JMP
+
+
